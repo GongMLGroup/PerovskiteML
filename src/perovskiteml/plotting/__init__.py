ea631176aeba7fd3705c3368db4e49781d4a8875
Violet Hart <vihart.rl@gmail.com>
2025-04-11 18:14:48 -0400

    Add plotting module

---
+++ src/perovskiteml/plotting/__init__.py
@@ -0,0 +1,10 @@
+from ._models import (
+    plot_actual_vs_predicted,
+    plot_feature_importance
+)
+
+__all__ = [
+    # Model Analysis
+    "plot_actual_vs_predicted",
+    "plot_feature_importance",
+]
