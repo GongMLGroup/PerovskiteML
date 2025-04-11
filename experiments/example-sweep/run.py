a31fe4f3e75374957b785568eb1c46987e2751a3
Violet Hart <vihart.rl@gmail.com>
2025-04-11 18:15:09 -0400

    update run with ExperimentConfig

--- experiments/example-sweep/run.py
+++ experiments/example-sweep/run.py
@@ -2,73 +2,21 @@ import os
 import sys
 import optuna
 import neptune.integrations.optuna as optuna_utils
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
 from datetime import datetime
 from pathlib import Path
+from perovskiteml.experiments import ExperimentConfig
 from perovskiteml.data import ExpandedDataset
-from perovskiteml.preprocessing.preprocessor import Preprocessor
-from perovskiteml.preprocessing import PrunerFactory
-from perovskiteml.utils.config_parser import load_config, config_to_neptune_format
-from perovskiteml.utils.parameter_sweep import ParameterSweepHandler
-from perovskiteml.logging import neptune_context
 from perovskiteml.models import ModelFactory
 from perovskiteml.models.base import BaseModelHandler
+from perovskiteml.preprocessing import PrunerFactory, Preprocessor
+from perovskiteml.logging import NeptuneConfig
+from perovskiteml.plotting import plot_feature_importance, plot_actual_vs_predicted
+from perovskiteml.utils import (
+    ParameterSweepHandler, OptunaSweepConfig, load_config, config_to_neptune_format
+)
+from perovskiteml.logging import neptune_context
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import root_mean_squared_error
-
-def log_artifacts(config, config_path, run):
-    if run:
-        run["config"] = config_to_neptune_format(config)
-        run["config/file"].upload(config_path)
-
-def plot_actual_vs_predicted(
-    config: dict,
-    y_train: pd.Series,
-    y_train_pred: pd.Series,
-    y_val: pd.Series,
-    y_val_pred: pd.Series
-):
-    # Plotting the results for the training set
-    fig = plt.figure(figsize=(4.5, 4))
-    plt.plot(y_train, y_train, color='black', label='Actual Values')
-    plt.scatter(y_train, y_train_pred, edgecolors='royalblue',
-                linewidth=1.2, color='royalblue', label='Train set')
-    plt.scatter(y_val, y_val_pred, edgecolors='deeppink',
-                linewidth=1.2, color='deeppink', label='Test set')
-    plt.title('Training Set: Actual vs Predicted')
-    plt.xlabel('Actual Values')
-    plt.ylabel('Predicted Values')
-    plt.legend()
-    # plt.grid(True)
-    plt.title(
-        f'{config["model"]["model_type"].upper()} Model: Actual vs Predicted Bandgap'
-    )
-    plt.tight_layout()
-    
-    return fig
-
-
-def plot_feature_importance(
-    model: BaseModelHandler,
-    X: pd.DataFrame
-):
-    feature_importance = model.model.feature_importances_
-    feature_names = list(X.columns)
-    importance_dict = {'feature': feature_names,
-                       'feature_importance': feature_importance}
-    importance_df = pd.DataFrame(importance_dict)
-    importance_df = importance_df.sort_values(
-        "feature_importance", ascending=False).reset_index()
-
-    fig = plt.figure(figsize=(4.5, 4))
-    sns.barplot(data=importance_df.head(20),
-                x='feature_importance', y='feature')
-    plt.title(
-        f'Feature importance Barplot for {model.config.model_type.upper()}')
-
-    return fig
 
 
 def create_run(
@@ -78,34 +26,45 @@ def create_run(
 ) -> tuple[BaseModelHandler, tuple, tuple]:
     
     config = config_sweep.suggested_config(trial)
+    experiment = ExperimentConfig.create_from_dict(config)
     dataset.reset_features()
+    
+    if experiment.pruning:
+        pruner = PrunerFactory.create(experiment.pruning)
+        pruner.prune(dataset)
         
-    pruner = PrunerFactory.create(config["pruning"])
-    preprocessor = Preprocessor(config["process"])
-    model = ModelFactory.create(config["model"])
-    if config["hyperparameters"]["pruning"]:
+    X, y = dataset.split_target()
+    if experiment.process:
+        preprocessor = Preprocessor(experiment.process)
+        X_transformed = preprocessor.preprocess(X, y)
+    else:
+        X_transformed = X
+    
+    model = ModelFactory.create(experiment.model)
+    if experiment.hyperparameters.pruning:
         model.init_callbacks(trial=trial)
 
-    pruner.prune(dataset)
-    X, y = dataset.split_target()
-    X_transformed = preprocessor.preprocess(X, y)
     X_train, X_val, y_train, y_val = train_test_split(
-        X_transformed, y, test_size=0.30, random_state=config["experiment"]["seed"]
+        X_transformed, y, test_size=0.30, random_state=experiment.seed
     )
     
     return model, (X_transformed, y), (X_train, y_train, X_val, y_val)
 
 def run(config_path):
     config_dict = load_config(config_path)
+    experiment_config = config_dict["experiment"]
+    logging_config = NeptuneConfig(**config_dict.get("logging", {}))
+    sweep_config = OptunaSweepConfig(**config_dict.get("hyperparameters", {}))
+    
     time_initialized = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     results_path = Path("results") / \
-        config_dict["experiment"]["name"] / time_initialized
+        experiment_config.get("name", "") / time_initialized
     study = optuna.create_study(
-        direction=config_dict["hyperparameters"]["direction"],
-        study_name=config_dict["hyperparameters"]["study_name"]
+        direction=sweep_config.direction,
+        study_name=sweep_config.study_name
     )
     config_sweep = ParameterSweepHandler(config_dict)
-    dataset = ExpandedDataset.cache_or_compute(config_dict["data"])
+    dataset = ExpandedDataset.cache_or_compute(**config_dict.get("data"))
 
     def objective(trial, config_sweep=config_sweep, dataset=dataset):
         model, _, split = create_run(trial, config_sweep, dataset)
@@ -114,7 +73,7 @@ def run(config_path):
         y_pred = model.predict(X_train)
         return root_mean_squared_error(y_pred, y_train)
 
-    with neptune_context(config_dict["logging"]) as neptune_run:
+    with neptune_context(logging_config) as neptune_run:
         if neptune_run:
             neptune_callback = optuna_utils.NeptuneCallback(
                 neptune_run,
@@ -127,7 +86,7 @@ def run(config_path):
         study.optimize(
             objective,
             callbacks=[neptune_callback] if neptune_callback else None,
-            n_trials=config_dict["hyperparameters"]["n_trials"]
+            n_trials=sweep_config.n_trials
         )
         
         print("Rerun Best Model")
@@ -142,16 +101,17 @@ def run(config_path):
         
         importance_plot = plot_feature_importance(model, data[0])
         residual_plot = plot_actual_vs_predicted(
-            config_dict, y_train, y_train_pred, y_val, y_val_pred
+            config_dict["model"]["model_type"], y_train, y_train_pred, y_val, y_val_pred
         )
         
-        log_artifacts(config_dict, config_path, neptune_run)
         if neptune_run:
+            neptune_run["config"] = config_to_neptune_format(config_dict)
+            neptune_run["config/file"].upload(config_path)
             neptune_run["plots/residual"].upload(residual_plot)
             neptune_run["plots/importance"].upload(importance_plot)
         
         # Local Saving
-        if config_dict["experiment"]["local_save"]:
+        if experiment_config.get("local_save", False):
             figure_path = results_path / "figures"
             os.makedirs(figure_path, exist_ok=True)
             model.save(results_path)
